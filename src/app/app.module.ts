1ab136b41dd9802898f0924473f1af3e336a6164
Augusto Yunes <atessmannyunes@gmail.com>
2021-04-28 13:49:09 -0300

    first project commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { TextElementComponent } from './text-element/text-element.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextElementComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/text-element/text-element.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-element',
+  templateUrl: './text-element.component.html',
+  styleUrls: ['./text-element.component.css']
+})
+export class TextElementComponent implements OnInit {
+
+  initialValue: number = 3000;
+  currentValue;
+  coinValue: number = 55000;
+  resultValue: number;
+
+  currencies:string[] = ["Bitcoin", "Ethereum", "Dogecoin", "blabla"]
+  selectedCurrency: string = this.currencies[0]
+
+  constructor() { }
+
+ ngOnInit() {
+    this.fetchCurrentValue(this.selectedCurrency);
+  }
+
+  //Adjust size of imput according to imput lenght
+  adjustInputSize(inputElement: HTMLInputElement){
+    inputElement.style.width =  ((inputElement.value.length + 1) * 15) + 'px';
+    if(inputElement.value.length == 0){
+      inputElement.style.width = (inputElement.getAttribute('placeholder').length + 1) * 13 + 'px'
+    }
+  }
+
+  //Validation function to check if imput are numbers
+  validateInput(value: number, inputElement: HTMLInputElement){
+    if(value / 1){
+      inputElement.classList.remove("error");
+      this.calcInvestment();
+    }else{
+      inputElement.classList.add("error");
+    }
+  }
+
+  //Updates the number of resultValue
+  calcInvestment(){
+    this.resultValue = (this.initialValue /  this.currentValue) * this.coinValue ;
+  }
+
+  //Function to mock the fetching of current coin value
+  async fetchCurrentValue(crypto: string){
+    switch (crypto) {
+      case "Bitcoin":
+        this.currentValue = await Promise.resolve(50000.00);
+        // this.coinValue = this.currentValue * 1.2;
+        this.calcInvestment();
+        break;
+      case "Ethereum":
+        this.currentValue = await Promise.resolve(1000.00);
+        // this.coinValue = this.currentValue * 1.2;
+        this.calcInvestment();
+        break;
+      case "Dogecoin":
+        this.currentValue = await Promise.resolve(50.00);
+        // this.coinValue = this.currentValue * 1.2;
+        this.calcInvestment();
+        break;
+      case "blabla":
+        this.currentValue = await Promise.resolve(3.00);
+        // this.coinValue = this.currentValue * 1.2;
+        this.calcInvestment();
+        break;
+      default:
+        this.currentValue = await Promise.resolve(0.00);
+        this.calcInvestment();
+        break;
+    }
+  }
+
+  onChangeSelect(){
+    console.log(this.selectedCurrency)
+    this.fetchCurrentValue(this.selectedCurrency);
+  }
+}
